61de16e73bebf3587745b75c82e8680b7e29188b
ajlopez <webmaster@ajlopez.com>
2020-03-13 07:15:25 -0300

    Compiler compiles code and text

--- test/compilers.js
+++ test/compilers.js
@@ -25,4 +25,17 @@ exports['compile text and expression'] = function (test) {
     test.equal(fn({ name: 'Adam' }), "hello, Adam");
 };
 
+exports['compile code and text'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const fn = compiler
+        .code("for (let k = 0; k < n; k++) {")
+        .text("hello")
+        .code("}")
+        .compile();
+        
+    test.equal(typeof fn, "function");
+    
+    test.equal(fn({ n: 3 }), "hellohellohello");
+};
 
