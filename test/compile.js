22adee38628e7f31ebcf94cb3af793c03eab928e
ajlopez <webmaster@ajlopez.com>
2020-03-20 11:26:55 -0300

    Compile text, expression and code

---
+++ test/compile.js
@@ -0,0 +1,27 @@
+
+const genco = require('..');
+
+exports['compile simple text'] = function (test) {
+    const result = genco.compile('hello, world');
+    
+    test.ok(result);
+    test.equal(typeof result, 'function');
+    test.equal(result(), 'hello, world');
+};
+
+exports['compile text and expression'] = function (test) {
+    const result = genco.compile('hello, ${name}');
+    
+    test.ok(result);
+    test.equal(typeof result, 'function');
+    test.equal(result({ name: 'Adam' }), 'hello, Adam');
+};
+
+exports['compile text and code'] = function (test) {
+    const result = genco.compile('hello, <# for (let k = 1; k <= 4; k++) {#>${k}<# } #>');
+    
+    test.ok(result);
+    test.equal(typeof result, 'function');
+    test.equal(result({ name: 'Adam' }), 'hello, 1234');
+};
+
