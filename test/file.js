6f77aa5d69459af43d2a6933607609f6cc98a4e9
ajlopez <webmaster@ajlopez.com>
2020-03-26 13:03:54 -0300

    Process file using model generates another file

--- test/file.js
+++ test/file.js
@@ -1,10 +1,31 @@
 
 const genco = require('..');
+
 const path = require('path');
+const fs = require('fs');
 
 exports['process simple file'] = function (test) {
     const filename = path.join(__dirname, 'files', 'hello.txt');
     const result = genco.file({ name: 'Adam' }, filename);
+    
+    test.ok(result);
+    test.equal(result, 'hello, Adam');
+};
+
+exports['process simple file creating a file'] = function (test) {
+    const filename = path.join(__dirname, 'files', 'hello.txt');
+    const filename2 = path.join(__dirname, 'result', 'hello.txt');
+    
+    try {
+        fs.statSysnc(filename2);
+        fs.unlinkSync(filename2);
+    }
+    catch (ex) {
+    }
+    
+    genco.file({ name: 'Adam' }, filename, filename2);
+    
+    const result = fs.readFileSync(filename2).toString();
     
     test.ok(result);
     test.equal(result, 'hello, Adam');
