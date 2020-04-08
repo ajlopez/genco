2d4458ad38f9be289a517f1de6f482411a1f152f
ajlopez <webmaster@ajlopez.com>
2020-04-08 06:55:20 -0300

    Lexer minor refactor

--- test/file.js
+++ test/file.js
@@ -4,12 +4,20 @@ const genco = require('..');
 const path = require('path');
 const fs = require('fs');
 
-exports['process simple file'] = function (test) {
+exports['process simple file with expression'] = function (test) {
     const filename = path.join(__dirname, 'files', 'hello.txt');
     const result = genco.file({ name: 'Adam' }, filename);
     
     test.ok(result);
     test.equal(result, 'hello, Adam');
+};
+
+exports['process simple file with code'] = function (test) {
+    const filename = path.join(__dirname, 'files', 'for.txt');
+    const result = genco.file({ n: 6 }, filename);
+    
+    test.ok(result);
+    test.equal(result, '012345');
 };
 
 exports['process simple file creating a file'] = function (test) {
